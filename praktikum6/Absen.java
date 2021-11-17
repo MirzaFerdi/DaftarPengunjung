d22c48ea9c1a7077f135d0159992c4c945cafc11
MirzaFerdi <92129602+MirzaFerdi@users.noreply.github.com>
2021-11-17 20:57:39 +0700

    Add files via upload

--- praktikum6/Absen.java
+++ praktikum6/Absen.java
@@ -6,13 +6,15 @@ import java.util.Scanner;
 public class Absen {
 
     ArrayList<Pengunjung> listPengunjung;
+   
+     ArrayList<String> namakota;
+    
 
     public Absen() {
         Scanner in = new Scanner(System.in);
         this.listPengunjung = new ArrayList<>();
-
+        
         while (true) {
-
             System.out.println("Nama Lengkap:");
             String namaLengkap = in.nextLine();
             if (namaLengkap.isEmpty()) {
@@ -22,19 +24,42 @@ public class Absen {
 
             System.out.println("Asal Kota:");
             String asalKota = in.nextLine();
-            if (asalKota.isEmpty()) {
-                System.out.println("Error: Tidak boleh kosong!");
-                continue;
+            if(!asalKota.contains("Kediri")) {
+                if(!asalKota.contains("Malang")) {
+                    if(!asalKota.contains("Blitar")) {
+                        if(!asalKota.contains("Probolinggo")) {
+                            if(!asalKota.contains("Pasuruan")) {
+                                if(!asalKota.contains("Mojokerto")) {
+                                    if(!asalKota.contains("Madiun")) {
+                                        if(!asalKota.contains("Surabaya")) {
+                                            if(!asalKota.contains("Batu")) {
+                                                System.out.println("Error: Nama kota harus di jawa timur");
+                                                continue;
+                                            }                    
+                                        }                    
+                                    }                    
+                                }                    
+                            }                    
+                        }                    
+                    }                    
+                }
             }
-
+            
             System.out.println("Nomor Ponsel:");
             String nomorPonsel = in.nextLine();
             if (!cekNomorPonsel(nomorPonsel)) {
                 System.out.println("Error: Format salah!");
                 continue;
             }
+            
+            System.out.println("Email:");
+            String Email = in.nextLine();
+            if (!cekEmail(Email)) {
+                System.out.println("Error: Format salah!");
+                continue;
+            }
 
-            this.listPengunjung.add(new Pengunjung(namaLengkap, asalKota, nomorPonsel));
+            this.listPengunjung.add(new Pengunjung(namaLengkap, asalKota, nomorPonsel,Email));
 
             System.out.println("Terimakasih\n");
 
@@ -44,35 +69,26 @@ public class Absen {
         }
     }
 
-    // Fungsi untuk mengecek format nomor ponsel
-    // Mengembalikan nilai true apabila format benar dan false apabila salah
+    
+    
     boolean cekNomorPonsel(String s) {
-         boolean valid = false;
-        
-        if(s.matches("^(\\+62|62|0)8[1-9]{6,10}$")) {
-            valid = true;
-        }
-
-        return valid;
+        return (s.matches("^(\\+62|62|0)8[1-9]{6,10}$"));
+    }
+    boolean cekEmail(String e) {
+        return (e.matches("\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b"));
     }
 
-    // Fungsi untuk mencetak semua daftar pengunjung dalam bentuk tabel pada konsol
-    // Contoh:
-    // +------------------+---------------+-------------------+
-    // | Nama Lengkap     | Asal Kota     | Nomor Ponsel      |
-    // +------------------+---------------+-------------------+
-    // | Budi Hartono     | Malang        | 1234512345        |
-    // | Iwan Sutrisno    | Banyuwangi    | 23456723456       |
-    // +------------------+---------------+-------------------+
+    
+    
     void cetakDaftarPengunjung() {
-        String formatcetak = "| %-23s | %-18s | %-18s |%n";
+        String formatcetak = "| %-23s | %-18s | %-18s | %-20s |%n";
         
-        System.out.format("+-------------------------+--------------------+--------------------+%n");
-        System.out.format("| Nama Lengkap            | Asal Kota          | Nomor Ponsel       |%n");
-        System.out.format("+-------------------------+--------------------+--------------------+%n");
+        System.out.format("+-------------------------+--------------------+--------------------+----------------------+%n");
+        System.out.format("| Nama Lengkap            | Asal Kota          | Nomor Ponsel       | Email                |%n");
+        System.out.format("+-------------------------+--------------------+--------------------+----------------------+%n");
         listPengunjung.forEach((var list) -> {
-            System.out.format(formatcetak, list.namaLengkap, list.asalKota, list.nomorPonsel);
+            System.out.format(formatcetak, list.namaLengkap, list.asalKota, list.nomorPonsel,list.Email);
         });
-        System.out.format("+-------------------------+--------------------+--------------------+%n");
+        System.out.format("+-------------------------+--------------------+--------------------+----------------------+%n");
     }
 }
--- praktikum6/Pengunjung.java
+++ praktikum6/Pengunjung.java
@@ -4,11 +4,14 @@ public class Pengunjung {
     public String namaLengkap;
     public String asalKota;
     public String nomorPonsel;
+    public String Email;
+    
 
-    public Pengunjung(String namaLengkap, String asalKota, String nomorPonsel) {
+    public Pengunjung(String namaLengkap, String asalKota, String nomorPonsel, String Email) {
         this.namaLengkap = namaLengkap;
         this.asalKota = asalKota;
         this.nomorPonsel = nomorPonsel;
+        this.Email = Email;
     }
     public String getnamaLengkap()
     {
@@ -23,6 +26,10 @@ public class Pengunjung {
     public String getnomorPonsel()
     {
         return nomorPonsel;
+    }
+    public String getEmail()
+    {
+        return Email;
     }
 }
 
